394ea31541677c2876e5e93438ded742fd386b86
Sabina Maria <p_sabinamaria@yahoo.com>
2023-03-15 14:08:26 +0200

    initial project structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1>My GitHub Projects</h1>;
+}
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
